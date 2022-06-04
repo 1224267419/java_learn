4d41d137a2cdfaec9aa2990b888a9a46c0ab09ef
1224267419 <1224267419@qq.com>
2022-06-04 17:02:13 +0800

    1

---
+++ 2. zhushi/java.java
@@ -0,0 +1,17 @@
+public class HelloJava{
+	public static void main(String[] args){
+		//单行注释
+		/*
+		多行
+		注释
+		*/
+		System.out.println("hello world");
+	}
+
+	/**
+		@author  lee
+		@version v1.0
+		这是我的第一个Java程序
+		通过此方法生成文档注释
+	 */
+}
